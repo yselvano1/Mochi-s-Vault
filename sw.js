3528abec386c65c07ac5b17b0e73b7e5212a2e3f
yselvano1 <yselvano1@gmail.com>
2026-08-12 15:34:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "v36.0"; // Bump versi ini setiap kali index.html berubah agar HP memperbarui cache
+const APP_VERSION = "v37.0"; // Bump versi ini setiap kali index.html berubah agar HP memperbarui cache
 const CACHE_NAME = "mochis-vault-" + APP_VERSION;
 
 // Aset same-origin: HARUS berhasil semua, ini yang bikin app tetap jalan offline.
